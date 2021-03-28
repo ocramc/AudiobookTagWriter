dad898aa8623709789fc2231697dcf64ed4e28db
Ocram <ocramc@gmail.com>
2021-03-28 00:04:38 +0000

    Added option path argument

--- tagwriter.py
+++ tagwriter.py
@@ -1,25 +1,65 @@
-import os, eyed3, pathlib, logging, untangle, mutagen
+import os, eyed3, pathlib, logging, untangle, mutagen, argparse, sys
 logging.getLogger().setLevel("ERROR")
-rootDir = os.getcwd()
+parser = argparse.ArgumentParser()
+parser.add_argument("--path", help="set working directory",
+                    dest="suppliedPath")
+
+args = parser.parse_args()
+
+print(args.suppliedPath)
+
+if args.suppliedPath != None:
+    logging.debug("Setting path to " + args.suppliedPath)
+    rootDir = args.suppliedPath
+else:
+    logging.debug("Setting path to " + os.getcwd())
+    rootDir = os.getcwd()
+
+if not os.path.exists(rootDir):
+    logging.error("Path " + rootDir + " not found.")
+    sys.exit(1)
+
+if not os.path.isdir(rootDir):
+    logging.error("The specified path is not a directory")
+    sys.exit(1)
+
+if not os.path.exists(os.path.join(rootDir , "metadata.db")):
+    logging.error("The specified directory is not a Calibre library")
+    sys.exit(1)
+
+reply = ""
+while reply != "y":
+    reply = str(input("Do you wish to update the metadata in " + rootDir + " (y/n): ")).lower().strip()
+    if reply == "y":
+        logging.debug("User confirmed metadata update")
+    elif reply == "n":
+        logging.debug("User cancelled metadata update")
+        sys.exit(1)
+    else:
+        print("Please specify either (y)es or (n)o to proceed")
+
+
+print("ye")
+
 totalCount = 0
 fileCount = 0
 
-fileTypes = ['.mp3', '.m4b']
+fileTypes = [".mp3", ".m4b"]
 
 for dirName, subdirList, fileList in os.walk(rootDir):
-    for fileName in fileList:
-        filePath = dirName + "\\" + fileName
+     for fileName in fileList:
+        filePath = os.path.join(dirName , fileName)
         fileExtension = pathlib.Path(filePath).suffix
         if fileExtension in fileTypes:
             totalCount += 1
 
 for dirName, subdirList, fileList in os.walk(rootDir):
     for fileName in fileList:
-        filePath = dirName + "\\" + fileName
+        filePath = os.path.join(dirName , fileName)
         fileExtension = pathlib.Path(filePath).suffix
         if fileExtension in fileTypes:
             fileCount += 1
-            print("Updating file " + str(fileCount) + " of " + str(totalCount) + " - " + fileName, end="\r")
+            print("Updating file ", str(fileCount), " of ", str(totalCount), " - ", fileName, end="\r")
             metadataFile = dirName + "\\metadata.opf"
             with open(metadataFile, "r", encoding="utf-8") as fd:
                 metadata = untangle.parse(fd.read())
@@ -27,7 +67,7 @@ for dirName, subdirList, fileList in os.walk(rootDir):
                 try:
                     bookTitle = metadata.package.metadata.dc_title.cdata
                     bookAuthor = metadata.package.metadata.dc_creator.cdata
-                
+
                 except:
                     logging.error("Unable to grab metadata for " + fileName)
                     pass
@@ -53,6 +93,6 @@ for dirName, subdirList, fileList in os.walk(rootDir):
                             audioFile.save(filePath)
                             logging.info("Updated metadata for " + fileName)
                             pass
-                        
+
                         except:
                             logging.error("Unable to update metadata for " + fileName)
